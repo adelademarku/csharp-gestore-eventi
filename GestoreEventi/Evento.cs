2ac56486860da5737ccbb15f52dd2653fa4096d2
adelademarku <adelademarku@email.com>
2022-12-13 12:43:00 +0100

    prova milestone 2 , mancano da controllare alcuni passaggi

--- GestoreEventi/Evento.cs
+++ GestoreEventi/Evento.cs
@@ -1,12 +1,170 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GestoreEventi
 {
-    internal class Evento
+    public class Evento
     {
+        //ATTRIBUTI 
+        private string titolo;
+        private string data;
+        private int capienzaMassimaEvento;
+        private int postiPrenotati;
+
+        //Costruttore
+        public Evento(string titolo, string data, int capienzaMassimaEvento, int postiPrenotati)
+        {
+            this.titolo = titolo;
+            this.data = data;
+            this.capienzaMassimaEvento = capienzaMassimaEvento;
+            this.postiPrenotati = postiPrenotati;
+            
+        }
+
+        //GETTERS
+
+        public string GetTitolo()
+        {
+            return titolo;
+        }
+        public string GetData()
+        {
+            return data;
+        }
+        public int GetCapienzaMassimaEvento()
+        {
+            return capienzaMassimaEvento;
+        }
+        public int GetPostiPrenotati()
+        {
+            return postiPrenotati;
+        }
+
+        //SETTERS
+
+        public string SetTitolo()
+        {
+            if (titolo != "")
+            {
+                return titolo;
+
+            }   else
+            {
+                throw new Exception("il titolo non puo essere omésso!");
+            } 
+        }
+
+        //controllo se la data non è passata 
+        public void SetData()
+        {
+            DateTime dataOraAttuale = DateTime.Now;
+            DateTime dataInserita = DateTime.Parse(data);
+            TimeSpan intervalloDiTempo = dataInserita - dataOraAttuale;
+
+            if (dataInserita < dataOraAttuale)
+            {
+                Console.WriteLine("La data inserita è nel passato!");
+                Console.WriteLine("In particolare la tua data sta " + intervalloDiTempo.Days + " giorni passati!");
+
+            }
+
+        }
+
+        public int SetPostiPrenotati()
+        { 
+                return postiPrenotati = 0;
+            
+        }
+
+
+
+        //controllo se la capienza massima di posti sia un numero positivo
+
+        public int SetCapienzaMasssimaEvento()
+        {
+            if (capienzaMassimaEvento >= 0)
+            {
+                return capienzaMassimaEvento;
+            } else
+            {
+                throw new Exception("la capacita massima non puo essere un numero negativo!");
+            }
+        }
+
+        //Mettodi 
+
+        //PrenotaPosti: aggiunge i posti passati come parametro ai posti prenotati.
+        //Se l’evento è già passato o non ha posti o non ha più posti disponibili deve sollevar un’eccezione.
+
+        public int PrenotaPosti(int posti)
+        {
+            if (postiPrenotati == capienzaMassimaEvento)
+            {
+                Console.WriteLine("Sold out!");
+                return capienzaMassimaEvento;
+
+            }
+            else if (postiPrenotati + posti < capienzaMassimaEvento)
+            {
+                Console.WriteLine("Numero dei posti prenotati: " + posti);
+                int postiAncoraDisponibili = capienzaMassimaEvento - postiPrenotati;
+                Console.WriteLine("Sono disponibili " + postiAncoraDisponibili + "posti");
+                return postiAncoraDisponibili;
+                
+            }
+            else if (postiPrenotati + posti > capienzaMassimaEvento)
+            {
+                throw new Exception("Stai prenotanto posti non disponibili!");
+            }
+            else return this.postiPrenotati;
+        }
+
+        //. DisdiciPosti: riduce del i posti prenotati del numero di posti indicati come parametro.
+        //Se l’evento è già passato o non ci sono i posti da disdire sufficienti, deve sollevare un’eccezione.
+
+        public int DisdiciPosti(int posti)
+        {
+            if (postiPrenotati - posti <= capienzaMassimaEvento)
+            {
+                return postiPrenotati = 0;
+                throw new Exception("non puoi disdire posti non prenotati!");
+            }
+            else
+            {
+                Console.WriteLine("Numero dei posti prenotati: " + (this.postiPrenotati - posti));
+                int postiAncoraDisponibili = capienzaMassimaEvento - postiPrenotati;
+                Console.WriteLine("Sono disponibili " + postiAncoraDisponibili + "posti");
+                return this.postiPrenotati - posti;
+            } 
+        }
+
+        //stampa
+
+        public override string ToString()
+        {
+            return "titolo" + this.titolo+ "\tdata: " + data+ "\t capienza massima evento: "+ capienzaMassimaEvento + "\tnumero dei posti prenotati:  "+ postiPrenotati;
+        }
+
+
+
+
+        public void StampaEvento()
+        {
+            Console.WriteLine(" ----------" + titolo+ " ----------");
+            Console.WriteLine("Data: \t" + data);
+            Console.WriteLine("Capacita Massima: \t" + capienzaMassimaEvento);
+            Console.WriteLine("Posti prenotati: \t" + this.postiPrenotati);
+         
+
+        }
+
+
+
+
     }
 }
--- GestoreEventi/Program.cs
+++ GestoreEventi/Program.cs
@@ -1,2 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using GestoreEventi;
+
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Inserisci il nome del evento: ");
+string nomeEvento = Console.ReadLine();
+Console.WriteLine("Inserisci la data del evento (gg/mm/yyyy): ");
+string dataEvento = Console.ReadLine();
+Console.WriteLine("Inserisci il numero dei posti totali: ");
+int capacitaMassimaEvento = Int16.Parse(Console.ReadLine());
+Console.WriteLine("Quanti posti desideri prenotare?: ");
+int postiPrenotati = Int16.Parse(Console.ReadLine());
+
+Evento evento = new Evento(nomeEvento, dataEvento, capacitaMassimaEvento, postiPrenotati);
+
+evento.StampaEvento();
+
+evento.GetPostiPrenotati();
+evento.GetCapienzaMassimaEvento();
+
+Console.WriteLine("Vuoi disdire dei posti (si/no) ? ");
+if (Console.ReadLine()== "si")
+{
+    Console.WriteLine("Indica il numero dei posti da disdire: ");
+    int numeriPostiDisdetti = Int16.Parse(Console.ReadLine());
+    evento.DisdiciPosti(numeriPostiDisdetti);
+    Console.WriteLine("Numero posti prenotati:" + postiPrenotati);
+    Console.WriteLine("Numero dei posti disdetti " +numeriPostiDisdetti);
+    Console.WriteLine("Numero dei posti disponibili " + (capacitaMassimaEvento - (postiPrenotati-numeriPostiDisdetti)));
+
+
+} else if (Console.ReadLine() == "no")
+{
+    Console.WriteLine("Ok. va bene");
+}
+
+
+
+
+
